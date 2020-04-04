cdb0cf2ebd6b8d3a3c2078e67afe2953ad150c2d
Thomas Humphries <t3humphr@uwaterloo.ca>
2020-04-03 21:37:44 -0400

    Comment Mac From Keyholder

--- Code/server/Keyholder.cpp
+++ Code/server/Keyholder.cpp
@@ -11,12 +11,12 @@ using namespace NTL;
 //     randoms_mac = __rands_mac;
 // }
 
-Keyholder::Keyholder(Context __c1, ZZ __key, ZZ __key_mac, ZZ __rands[], ZZ __rands_mac[]){
+Keyholder::Keyholder(Context __c1, ZZ __key, ZZ __rands[]){
     public_context = __c1;
     key = __key;
-    key_mac = __key_mac;
+    //key_mac = __key_mac;
     randoms = __rands;
-    randoms_mac = __rands_mac;
+    //randoms_mac = __rands_mac;
 }
 
 // Keyholder::Keyholder(Context __c2, ZZ __key, ZZ __key_mac, ZZ __rands[], ZZ __rands_mac[]){
@@ -39,12 +39,12 @@ void Keyholder::initialize_context(Context __c1){
     public_context = __c1;
     ZZ_p::init(public_context.p-1);
     key = rep(random_ZZ_p());
-    key_mac = rep(random_ZZ_p());
+    //key_mac = rep(random_ZZ_p());
     randoms = new ZZ[public_context.t]; //TODO this is wrong should be t-1??
-    randoms_mac = new ZZ[public_context.t];
+    //randoms_mac = new ZZ[public_context.t];
     for (int i=0;i<public_context.t;i++){
         randoms[i] = rep(random_ZZ_p());
-        randoms_mac[i] = rep(random_ZZ_p());
+        //randoms_mac[i] = rep(random_ZZ_p());
     }
 }
 
@@ -60,12 +60,12 @@ void Keyholder::initialize_from_file(Context context, string filename){
     // t = jsonFile["t"] ;
     int t = public_context.t;
     key = ZZ(INIT_VAL, jsonFile["key"].get<string>().c_str());
-    key_mac = ZZ(INIT_VAL, jsonFile["key_mac"].get<string>().c_str());
+    //key_mac = ZZ(INIT_VAL, jsonFile["key_mac"].get<string>().c_str());
     randoms = new NTL::ZZ[t-1];
-    randoms_mac = new NTL::ZZ[t-1];
+    //randoms_mac = new NTL::ZZ[t-1];
     for(int i = 0 ; i < t-1 ; i++){
         randoms[i] = ZZ(INIT_VAL, jsonFile["randoms"][i].get<string>().c_str());
-        randoms_mac[i] = ZZ(INIT_VAL, jsonFile["randoms_mac"][i].get<string>().c_str());
+       // randoms_mac[i] = ZZ(INIT_VAL, jsonFile["randoms_mac"][i].get<string>().c_str());
     }
 }
 
@@ -79,7 +79,6 @@ Scheme1_Round1_receive Keyholder::Scheme1_Round1(Scheme1_Round1_send payload){
 	conv(g_p, g);
     conv(h_x_alpha, payload.h_x_alpha);
     conv(g_alpha, payload.g_alpha);        
-
 	ZZ_p R, R_inverse, R_alpha;
     {
 		ZZ_pPush push(p-1);
@@ -101,15 +100,15 @@ Scheme1_Round1_receive Keyholder::Scheme1_Round1(Scheme1_Round1_send payload){
     // conv(R_inverse, __R_inverse);
 
 	R_alpha = NTL::power(g_alpha, r);
-    output.masked_secret_alpha = rep(R_alpha * NTL::power(h_x_alpha, key));
+    output.masked_secret_alpha = ZZ(0);//TODO double check this rep(R_alpha * NTL::power(h_x_alpha, key));
 	for (int i = 0; i < t-1; i++){
 		output.masked_coefficients_alpha.push_back(rep(R_alpha * NTL::power(h_x_alpha, randoms[i])));
 	}
 
-    output.masked_mac_alpha = rep(R_alpha * NTL::power(h_x_alpha, key));
-	for (int i = 0; i < t-1; i++){
-		output.masked_mac_coefficients_alpha.push_back(rep(R_alpha * NTL::power(h_x_alpha, randoms_mac[i])));
-	}
+ //    output.masked_mac_alpha = rep(R_alpha * NTL::power(h_x_alpha, key));
+	// for (int i = 0; i < t-1; i++){
+	// 	output.masked_mac_coefficients_alpha.push_back(rep(R_alpha * NTL::power(h_x_alpha, randoms_mac[i])));
+	// }
 
     return output;
 }
@@ -121,19 +120,19 @@ Scheme1_Round2_receive Keyholder::Scheme1_Round2(Scheme1_Round2_send payload){
     Scheme1_Round2_receive output;
 	
 	ZZ_to_mpz_t(__mpz_temp, __R_inverse);
-	pcs_ep_mul(payload.pk, output.mpz_secret, payload.mpz_secret, __mpz_temp);
-	pcs_ep_mul(payload.pk, output.mpz_mac, payload.mpz_mac, __mpz_temp);
+	pcs_ep_mul(payload.pk, output.mpz_secret, payload.mpz_secret, __mpz_temp);//TODO do we need this?
+	//pcs_ep_mul(payload.pk, output.mpz_mac, payload.mpz_mac, __mpz_temp);
 	
     ZZ_p::init(public_context.p);
 	ZZ_p R_inv_id_pows = to_ZZ_p(payload.id) * conv<ZZ_p>(__R_inverse);
 	for (int i=0;i<public_context.t-1;i++){
 		ZZ_p_to_mpz_t(__mpz_temp, R_inv_id_pows);
 		pcs_ep_mul(payload.pk, (payload.mpz_coefficients)[i], (payload.mpz_coefficients)[i], __mpz_temp);
-		pcs_ep_mul(payload.pk, (payload.mpz_mac_coefficients)[i], (payload.mpz_mac_coefficients)[i], __mpz_temp);
+		//pcs_ep_mul(payload.pk, (payload.mpz_mac_coefficients)[i], (payload.mpz_mac_coefficients)[i], __mpz_temp);
 		R_inv_id_pows = R_inv_id_pows * to_ZZ_p(payload.id);
 
 		pcs_ee_add(payload.pk, output.mpz_secret, output.mpz_secret, (payload.mpz_coefficients)[i]);
-		pcs_ee_add(payload.pk, output.mpz_mac, output.mpz_mac, (payload.mpz_mac_coefficients)[i]);
+		//pcs_ee_add(payload.pk, output.mpz_mac, output.mpz_mac, (payload.mpz_mac_coefficients)[i]);
 	}
 
     return output;
@@ -149,25 +148,25 @@ Scheme2_receive Keyholder::Scheme2_Round1(Scheme2_send payload){
 		ZZ_pPush push(p-1);
 		// ZZ_p::init(q);
 		ZZ_p __secret_exp, __mac_exp, id_pows, __temp, __temp_id;
-		conv(__secret_exp, key);
-		conv(__mac_exp, key);
+		__secret_exp = ZZ_p(0);//TODO Double check conv(__secret_exp, key);
+		//conv(__mac_exp, key);
 		conv(id_pows, payload.id);
         conv(__temp_id, payload.id);
 		for (int i = 0; i < t-1; i++){
 			conv(__temp, randoms[i]);
 			__secret_exp += id_pows * __temp;
-			conv(__temp, randoms_mac[i]);
-			__mac_exp += id_pows * __temp;
+			//conv(__temp, randoms_mac[i]);
+			//__mac_exp += id_pows * __temp;
 			id_pows *= __temp_id;
 		}
 		secret_exp = rep(__secret_exp);
-		mac_exp = rep(__mac_exp);
+		//mac_exp = rep(__mac_exp);
 	}
 
     Scheme2_receive output;
 
 	output.secret_share_alpha = rep(NTL::power(conv<ZZ_p>(payload.h_x_alpha), secret_exp));
-	output.mac_share_alpha= rep(NTL::power(conv<ZZ_p>(payload.h_x_alpha), mac_exp));
+	//output.mac_share_alpha= rep(NTL::power(conv<ZZ_p>(payload.h_x_alpha), mac_exp));
 
     return output;
 }
@@ -194,8 +193,8 @@ string Keyholder::toString()  //TODO these are likely much larger than needed.
 
     toReturn += ZZ_to_str(key);
     toReturn += delim;
-    toReturn += ZZ_to_str(key_mac);
-    toReturn += delim;
+    //toReturn += ZZ_to_str(key_mac);
+    //toReturn += delim;
 
     int size = public_context.t-1;
 
@@ -204,11 +203,11 @@ string Keyholder::toString()  //TODO these are likely much larger than needed.
         toReturn += ZZ_to_str(randoms[i]);
         toReturn += delim;
     }
-    for(int i = 0 ; i<size ; i++)
-    {
-        toReturn += ZZ_to_str(randoms_mac[i]);
-        toReturn += delim;
-    }
+    // for(int i = 0 ; i<size ; i++)
+    // {
+    //     toReturn += ZZ_to_str(randoms_mac[i]);
+    //     toReturn += delim;
+    // }
 
     toReturn += ZZ_to_str(r);
     toReturn += delim;
@@ -242,8 +241,8 @@ Keyholder::Keyholder(string str)
 
     std::getline(ss, token, delim);
     key = str_to_ZZ(token);
-    std::getline(ss, token, delim);
-    key_mac = str_to_ZZ(token);
+    //std::getline(ss, token, delim);
+    //key_mac = str_to_ZZ(token);
 
     int size = public_context.t-1;
     randoms = new ZZ[size];
@@ -252,12 +251,12 @@ Keyholder::Keyholder(string str)
         std::getline(ss, token, delim); 
         randoms[i] = str_to_ZZ(token);
     }
-    randoms_mac = new ZZ[size];
-    for(int i = 0 ; i<size ; i++)
-    {
-        std::getline(ss, token, delim); 
-        randoms_mac[i] = str_to_ZZ(token);
-    }
+    // randoms_mac = new ZZ[size];
+    // for(int i = 0 ; i<size ; i++)
+    // {
+    //     std::getline(ss, token, delim); 
+    //     randoms_mac[i] = str_to_ZZ(token);
+    // }
 
     std::getline(ss, token, delim);
     r = str_to_ZZ(token);
--- Code/server/Keyholder.h
+++ Code/server/Keyholder.h
@@ -10,12 +10,12 @@ class Keyholder{
 	public:
 	Context public_context;
 	ZZ key;
-	ZZ key_mac;
+	//ZZ key_mac;
 	ZZ* randoms;
-	ZZ* randoms_mac;
+	//ZZ* randoms_mac;
 	ZZ r, __R, __R_inverse;
 
-    Keyholder(Context __c1, ZZ __key, ZZ __key_mac, ZZ __rands[], ZZ __rands_mac[]);
+    Keyholder(Context __c1, ZZ __key, ZZ __rands[]);
 	Keyholder(Context __c1);
 	Keyholder(string str);
 	Keyholder(){}
